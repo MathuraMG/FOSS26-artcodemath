0b67cc2a3374a778e065739c5869dd6616e55e50
Aditi Bhat <241776346+spinfinity-adt@users.noreply.github.com>
2026-09-24 01:03:50 +0530

    Cleaning up Truchet Tiles code

--- Truchet-Tiles/sketch.js
+++ Truchet-Tiles/sketch.js
@@ -1,31 +1,26 @@
 let x, y;
-let size,choice;
+let size, choice;
 function setup() {
   createCanvas(400, 400);
-  
+
   size = 40;
-  frameRate(2)
-  angleMode(DEGREES)
+  frameRate(1);
+  angleMode(DEGREES);
+  strokeWeight(2);
+  noFill();
 }
 
 function draw() {
-  background("MidnightBlue");
-  
-  noStroke()
-  for (x = size / 2; x < 400; x += size) {
-    for (y = size / 2; y < 400; y += size) {
-      //ellipse(x, y, size, size);
-      choice=random(0,1);
-      stroke(x,255-y,y)
-      strokeWeight(14)
-      noFill();
-      if(choice<0.5){
-      arc(x-size/2,y-size/2,size,size,0,90)
-  arc(x+size/2,y+size/2,size,size,180,270)
-        }
-      else{
-       arc(x+size/2,y-size/2,size,size,90,180)
-  arc(x-size/2,y+size/2,size,size,270,0)
+  background("white");
+  for (x = 0; x < width; x += size) {
+    for (y = 0; y < height; y += size) {
+      choice = random(0, 1);
+      if (choice < 0.5) {
+        arc(x, y, size, size, 0, 90);
+        arc(x + size, y + size, size, size, 180, 270);
+      } else {
+        arc(x + size, y, size, size, 90, 180);
+        arc(x, y + size, size, size, 270, 0);
       }
     }
   }
@@ -35,6 +30,6 @@ function keyPressed() {
     saveCanvas("truchet-tiles.png");
   }
   if (key == "g") {
-    saveGif("truchet-tiles", 5);
+    saveGif("truchet-tiles.gif", 5);
   }
 }
--- arc-10print/sketch.js
+++ arc-10print/sketch.js
@@ -2,36 +2,26 @@ let x, y;
 let size, choice;
 function setup() {
   createCanvas(400, 400);
-  size = 40;
+  size = 50;
   frameRate(2);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  background("MidnightBlue");
-
+  background("black");
   noStroke();
-  for (x = size / 2; x < 400; x += size) {
-    for (y = size / 2; y < 400; y += size) {
-      //ellipse(x, y, 50, 50);
+  for (x = 0; x < width; x += size) {
+    for (y = 0; y < height; y += size) {
       choice = random(0, 1);
-      fill(255, x, y, random(80, 255));
+      fill(255 - x, y, x, random(50, 200));
       if (choice < 0.25) {
-       arc(x - size / 2, y - size / 2, size * 2, size * 2, 0, HALF_PI);
-      } else if (choice > 0.25 && choice < 0.5) {
-      arc( x - size / 2, y + size / 2, size * 2,      size * 2, PI + HALF_PI,  2 * PI  );
-      }
-      else if (choice > 0.5 && choice < 0.75) {
-        arc(
-          x + size / 2,
-          y + size / 2,
-          size * 2,
-          size * 2,
-          PI,
-           PI + HALF_PI
-        );
-      }
-      else {
-       arc(x + size / 2, y - size / 2, size * 2, size * 2, HALF_PI, PI);
+        arc(x, y, size * 2, size * 2, 0, 90);
+      } else if (choice < 0.5) {
+        arc(x + size, y, size * 2, size * 2, 90, 180);
+      } else if (choice < 0.75) {
+        arc(x + size, y + size, size * 2, size * 2, 180, 270);
+      } else {
+        arc(x, y + size, size * 2, size * 2, 270, 360);
       }
     }
   }
