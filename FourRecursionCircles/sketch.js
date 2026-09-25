d5fe61ce5507958c3ee846ec83eea6b0406555e6
Mathura MG <mmg9291@gmail.com>
2026-09-25 09:51:36 +0530

    add more examples

--- FourRecursionCircles/sketch.js
+++ FourRecursionCircles/sketch.js
@@ -1,22 +1,26 @@
+let slider;
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(400, 400);
+  slider = createSlider(1,7,3,1);
+  slider.position(10, 370);
+  slider.size(380);
 }
 
 function draw() {
-  background(220);
-  drawCircles(width / 2, height / 2, width/2);
-  noLoop();
+  background(0);
+  let order = slider.value();
+  drawCircles(width / 2, height / 2, width/2,order);
 }
 
-function drawCircles(x, y, radius) {
-  stroke(0);
-  noFill();
+function drawCircles(x, y, radius,order) {
+  noStroke();
+  fill(255,40);
   circle(x, y, radius * 2);
-  if (radius > 16) {
+  if (order > 1) {
     // drawCircle() calls itself four times.
-    drawCircles(x + radius / 2, y, radius / 2);
-    drawCircles(x - radius / 2, y, radius / 2);
-    drawCircles(x, y + radius / 2, radius / 2);
-    drawCircles(x, y - radius / 2, radius / 2);
+    drawCircles(x + radius / 2, y, radius / 2, order-1);
+    drawCircles(x - radius / 2, y, radius / 2, order-1);
+    drawCircles(x, y + radius / 2, radius / 2, order-1);
+    drawCircles(x, y - radius / 2, radius / 2, order-1);
   }
 }
